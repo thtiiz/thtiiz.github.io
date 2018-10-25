6aaec6c32b1ce35604b80e469b12e8744ffe6ed1
thtiiz <napon-na123@hotmail.com>
2018-10-25 13:22:57 +0700

    Bootstrapgit add .

--- src/App.js
+++ src/App.js
@@ -10,6 +10,7 @@ class App extends Component {
           <img src={logo} className="App-logo" alt="logo" />
           <p>
             Hello World!!!
+            <div className="alert alert-primary">pasdfsdf</div>
           </p>
           <a
             className="App-link"
